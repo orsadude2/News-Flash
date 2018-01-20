32d1ca60d77ca75fafb19700268134a7992120ff
Ovidiu Parasca <OParasca@ApolloRoboticsTechnology.com>
2018-01-20 10:30:42 -0800

    - added the UI portion to the Home Feed (list view, navigation bar, and hamburger menu)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,27 @@
 'use strict';
 
-$(document).ready(function() {
-  console.log('ready!');
-});
+var app = app || {};
+var Article = 
 
-$.getJSON('data.json', function(json) {
-  console.log(json); 
+(module => {
+
+const articlePage = {};
+
+var loadData = function (url, id, anchor) {
+  $.getJSON(url, function(data) {
+    var template = $(id).html();
+    var articles = Handlebars.compile(template)(data);
+    $(anchor).append(articles);
+  });
+};
+loadData('data.json', '#feedView-template', '#anchor');
+
+module.articlePage = articlePage;
+
+}) (app);
+
+$(document).ready(function() {
+  $('#navbar-toggle').on('click', function() {
+    $('#nav-links').slideToggle();
+  });
 });
