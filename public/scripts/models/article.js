972feea23bc299fee7e9da7204b42c2c44424360
AaronBruce <aaron.bruce@comcast.net>
2018-01-20 10:29:26 -0800

    added functionale for pulling data from API url, storing it into an object, articles, and rendering it to the DOM via markup. (built up route.js, main-view.js, server.js, and article.js)

--- public/scripts/models/article.js
+++ public/scripts/models/article.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // 1) fetchAll (anon vs user)
 // 2) login
 // 3) sign-up
@@ -6,38 +8,74 @@
 // var app = app || {};
 
 // (module => {
-//     const __API_URL__ = 'https://dk-rm-nd-booklist.herokuapp.com/api/v1/books';
-//     // const __API_URL__ = 'http://localhost:3000/api/v1/books';
-    
-//     function Book() {
+//     const __API_URL__ = 'https://dk-rm-nd-Articlelist.herokuapp.com/api/v1/Articles';
+//     // const __API_URL__ = 'http://localhost:3000/api/v1/Articles';
+
+//     function Article() {
 
 //     }
 
-//     Book.all = [];
+//     Article.all = [];
 
-//     Book.fetchAll = () => $.getJSON(__API_URL__);
+//     Article.fetchAll = () => $.getJSON(__API_URL__);
 
-//     Book.fetchOne = id => $.getJSON(`${__API_URL__}/${id}`);
+//     Article.fetchOne = id => $.getJSON(`${__API_URL__}/${id}`);
 
-//     Book.create = book => $.post(__API_URL__, book);
+//     Article.create = Article => $.post(__API_URL__, Article);
 
-//     Book.update = book => {
+//     Article.update = Article => {
 //         return $.ajax({
-//             url: __API_URL__ + '/' + book.id,
+//             url: __API_URL__ + '/' + Article.id,
 //             method: 'PUT',
-//             data: book,
+//             data: Article,
 //         }).catch(error => console.error(error))
-        
+
 //     }
 
-//     Book.delete = id => {
-//         console.log('books ' + id)
+//     Article.delete = id => {
+//         console.log('Articles ' + id)
 //         return $.ajax({
 //             url: __API_URL__ + '/' + id,
 //             method: 'DELETE'
 //         }).catch(error => console.error(error))
 //     }
-        
-//     module.Book = Book;
+
+//     module.Article = Article;
 
 // })(app);
+
+
+var app = app || {};
+
+(module => {
+
+  const API_URL = '/sources';
+
+
+  const Article = {};
+
+  Article.all = [];
+
+  Article.fetchAll = () => {
+    console.log('fetchAll', API_URL)
+    return $.getJSON(API_URL).then(things => {
+      Article.all = things
+      console.log('things', things);
+      // Article.all.sort((a, b) => {
+      //   if (a.name > b.name) {
+      //     return 1
+      //   } else if (a.name < b.name) {
+      //     return -1
+      //   } else {
+      //     return 0
+      //   }
+      // })
+    }).catch(error => console.error(error));
+  }
+
+
+
+
+
+  module.Article = Article;
+})(app)
